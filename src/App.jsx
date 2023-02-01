7c1f203e9666e463c56d7977d564320719007020
matkronh <mats.kodehode@gmail.com>
2023-02-01 12:40:31 +0100

    hey hey

--- src/App.jsx
+++ src/App.jsx
@@ -2,31 +2,42 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import './App.css'
 
+import { AiFillHtml5 } from 'react-icons/ai';
+import { DiReact } from 'react-icons/di'
+import { SiCss3 } from 'react-icons/si'
+import { IoLogoJavascript } from 'react-icons/io'
+
+
+function Profile(){
+  return (
+      <div>
+        <img id="resizeMe" src="https://github.com/Matkronh/third-project/blob/master/img/doggi.jpg?raw=true"></img>
+        <br />
+        <text>Name: John Dog</text>
+        <br />
+        <text>Short: Web Developer extraordinaire</text>
+      </div>
+  )
+}
+
+function Icons(){
+  return (
+    <div id="icon-box">
+      <div className='item'><AiFillHtml5 /></div>
+      <div className='item'><DiReact /></div>
+      <div className='item'><SiCss3 /></div>
+      <div className='item'><IoLogoJavascript /></div>
+    </div>
+  )
+}
+
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+    <div className="card">
+        <Profile />
+        <Icons />
     </div>
   )
 }
