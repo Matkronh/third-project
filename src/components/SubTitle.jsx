8737218fea28b51643388fa30769dd2307f9dfb9
matkronh <mats.kodehode@gmail.com>
2023-02-03 09:02:56 +0100

    dsa

---
+++ src/components/SubTitle.jsx
@@ -0,0 +1,14 @@
+import styles from './SubTitle.module.css'
+
+
+
+function SubTitle(){
+    return (
+    <div className={styles.divtest}>
+        <p className={styles.htest}>Check out these lovely dogs in your local area</p>
+    </div>
+    )
+}
+
+
+export default Title
--- src/components/Title.jsx
+++ src/components/Title.jsx
@@ -5,8 +5,7 @@ console.log(styles)
 function Title(){
     return (
     <div className={styles.divtest}>
-        <h1 className={styles.h1test}>Extra ordinary dogs in your area</h1>
-        <p className={styles.h2test}>Check out these lovely dogs in your local area</p>
+        <h1 className={styles.htest}>Extra ordinary dogs in your area</h1>
     </div>
     )
 }
